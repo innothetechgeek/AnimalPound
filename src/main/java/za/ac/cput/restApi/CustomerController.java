8b65bec54bdd25fdf1d7d310e52e46ac0cdee11e
innothetechgeek <innothetechgeek@gmail.com>
2016-09-04 06:37:03 +0200

    Update CustomerController.java

--- src/main/java/za/ac/cput/restApi/CustomerController.java
+++ src/main/java/za/ac/cput/restApi/CustomerController.java
@@ -1,5 +1,11 @@
 package za.ac.cput.restApi;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
